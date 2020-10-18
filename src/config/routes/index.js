0dadef0bcf1d545ff9189341c788704bec53befa
Dmitriy <patrickjsmirnov@gmail.com>
2020-10-18 20:08:40 +0300

    fix url

--- src/config/routes/index.js
+++ src/config/routes/index.js
@@ -2,7 +2,7 @@ const ENV = process.env.NODE_ENV
 const port = 3000
 
 const DEV_HOST = `http://localhost:${port}`
-const PROD_HOST = `http://81.163.28.64/:${port}`
+const PROD_HOST = `http://81.163.28.64:${port}`
 const HOST = ENV === 'development' ? DEV_HOST : PROD_HOST
 
 export { HOST }
